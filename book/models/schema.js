219fede2f7f81afa18ff68655c83d98f06679dbf
Pujitha Pulibandla <55695393+PujithaNaidu@users.noreply.github.com>
2021-04-02 10:40:57 +0530

    deleted folder

--- book/models/schema.js
+++
@@ -1,6 +0,0 @@
-const knex = require('../config/config').knex;
-const bookshelf = require('bookshelf')(knex);
-const cities=bookshelf.Model.extend({
-    tableName:'cities'
-});
-module.exports=cities;
